e237e666ea2b8a140ec7be850c27933f142fd230
Lourd <z1309014381@gmail.com>
2019-11-22 06:48:49 +0800

    添加地图

--- src/views/home/Map.vue
+++ src/views/home/Map.vue
@@ -1,20 +1,29 @@
 <template>
-  <baidu-map
-    class="bm-view"
-    style="display: flex; flex-direction: column"
-    :center="center"
-    :zoom="15"
-  >
+  <baidu-map class="bm-view"
+             style="display: flex; flex-direction: column"
+             :center="center"
+             :zoom="15">
     <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
     <bm-view style="width: 100%; height:100px; flex: 1"></bm-view>
+    <bm-marker v-for="(item,index) in enterprises"
+               :key="index"
+               :position="{lng: item.baiduLocation[1], lat: item.baiduLocation[0]}"
+               :dragging="false"
+               animation="BMAP_ANIMATION_BOUNCE">
+      <bm-label :content="item.enterprise.alias"
+                :labelStyle="{color: 'red', fontSize : '8px'}"
+                :offset="{width: -35, height: 30}" />
+    </bm-marker>
   </baidu-map>
 </template>
 
 <script>
+import enterprises from './enterprises.js'
 export default {
   data () {
     return {
-      center: { lng: 111.2772301, lat: 40.07128 }
+      center: { lng: 111.2772301, lat: 40.07128 },
+      enterprises: enterprises
     }
   }
 }
---
+++ src/views/home/enterprises.js
@@ -0,0 +1,113 @@
+export default [
+  {
+    warn: true,
+    googleLoaction: [40.053537, 111.234384],
+    baiduLocation: [40.05929088276406, 111.24097618058673],
+    enterprise: {
+      warnNumber: 5,
+      name: '内蒙古伊泰煤制油有限公司',
+      alias: '伊泰煤制油',
+      industry: '化工行业',
+      address: '内蒙古自治区鄂尔多斯市准格尔旗大路工业园区南煤化工基地纬一路南侧',
+      legal: '宁永明',
+      phone: '15947070897',
+      dfu: '2739.72',
+      efc: '3781.93',
+      defc: '1.4',
+      upd: '9'
+    }
+  },
+  {
+    googleLoaction: [40.04884, 111.27275],
+    baiduLocation: [40.054622448609834, 111.279331548612],
+    enterprise: {
+      name: '久泰能源内蒙古有限公司',
+      alias: '久泰能源',
+      industry: '燃料加工业',
+      dfu: '1369.86',
+      efc: '1294.29',
+      defc: '0.95',
+      upd: '10'
+    }
+  },
+  {
+    googleLoaction: [40.0507859767, 111.2709469159],
+    baiduLocation: [40.0577881501, 111.2840821359],
+    enterprise: {
+      name: '中科合成油内蒙古有限公司',
+      alias: '中科合成油',
+      industry: '燃料加工业',
+      dfu: '109.5',
+      efc: '8.65',
+      defc: '0.08',
+      upd: '12'
+    }
+  },
+  {
+    googleLoaction: [40.054063, 111.293478],
+    baiduLocation: [40.05986240177421, 111.3000537871066],
+    enterprise: {
+      name: '内蒙古天润化肥股份有限公司',
+      alias: '天润化肥',
+      industry: '化工行业',
+      dfu: '657.52',
+      efc: '1063.332',
+      defc: '1.62',
+      upd: '31'
+    }
+  },
+  {
+    googleLoaction: [40.053209, 111.286869],
+    baiduLocation: [40.05900291640226, 111.29344670787664],
+    enterprise: {
+      name: '西北能化有限公司',
+      alias: '西北能化',
+      industry: '燃料加工业',
+      dfu: '136.98',
+      efc: '1086.21',
+      defc: '7.93',
+      upd: '13'
+    }
+  },
+  {
+    googleLoaction: [40.0535415522, 111.3018198025],
+    baiduLocation: [40.0592206944, 111.3084356292],
+    enterprise: {
+      name: '易高煤化有限公司',
+      alias: '易高煤化',
+      industry: '燃料加工业',
+      dfu: '164.37',
+      efc: '1942.16',
+      defc: '11.82',
+      upd: '8'
+    }
+  },
+  {
+    warn: true,
+    googleLoaction: [40.036158, 111.279573],
+    baiduLocation: [40.04194562570444, 111.28615181703005],
+    enterprise: {
+      warnNumber: 3,
+      name: '伊东东华有限公司',
+      alias: '伊东东华能源',
+      industry: '化工行业',
+      dfu: '2739.72',
+      efc: '3781.93',
+      defc: '1.38',
+      upd: '42'
+    }
+  },
+  {
+    googleLoaction: [40.06615, 111.330986],
+    baiduLocation: [40.07198227233622, 111.33755074778401],
+    enterprise: {
+      name: '内蒙古晶阳能源有限公司',
+      alias: '晶阳能源',
+      industry: '金属材料工业',
+      dfu: '287.66',
+      efc: '625.77',
+      defc: '2.18',
+      upd: '23'
+    }
+  }
+]
